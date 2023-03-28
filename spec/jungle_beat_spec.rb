606f56c7c69f0231d807d1cbac40a716f3f341b1
Jesse Sorman <jessesorman@gmail.com>
2023-03-28 17:18:03 -0400

    add play test

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -8,7 +8,15 @@ RSpec.describe JungleBeat do
     jb = JungleBeat.new
     expect(jb.list.head).to eq(nil)
     jb.append("deep doo ditt")
+    expect(jb.list.head.data).to eq("deep")
+    expect(jb.list.head.next_node.data).to eq("doo")
+    expect(jb.append).to eq("woo hoo shu")
+    expect(jb.count).to eq(6)
     # require 'pry'; binding.pry
   end
-
+  it "plays" do
+    jb = JungleBeat.new
+    jb.append("deep doo ditt woo hoo shu")
+    jb.play
+  end
 end
