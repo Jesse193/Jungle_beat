8b1e5e3e9ab3b0c674756c2ec27e84c6a183978d
Jesse Sorman <jessesorman@gmail.com>
2023-03-28 17:17:27 -0400

    add append method

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,7 +1,27 @@
 class JungleBeat
-  attr_reader :list, :head, :LinkedList, :append, :Node
+  attr_reader :list, :head, :LinkedList, :append, :Node, :data
   def initialize
-    @list = LinkedList
+    @list = LinkedList.new
     @head = head
+    @counter = 0
+    @data = data
   end
 end
+
+def append(data)
+  current_node = head
+  if @head == nil
+    @head = Node.new(data)
+  else
+    while current_node.next_node != nil
+      current_node = current_node.next_node
+    end
+    current_node.next_node = Node.new(data)
+  end
+  count
+end 
+def count
+  @counter
+end
+
+  
